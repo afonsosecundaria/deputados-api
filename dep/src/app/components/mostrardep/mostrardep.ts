ebb476a55a556e6c7b9597ef893c35a000a9bb3a
Afonso Silva de Lima Torres <20231144010009@ifrn.local>
2026-08-18 15:17:42 -0300

    abc

--- dep/src/app/components/mostrardep/mostrardep.ts
+++ dep/src/app/components/mostrardep/mostrardep.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { DeputadoService } from '../../services/deputado.service';
 import { Deputado } from '../../model/deputados.model';
 
@@ -11,10 +11,31 @@ import { Deputado } from '../../model/deputados.model';
 export class Mostrardep {
   readonly #deputadoService = inject(DeputadoService);
   protected deputados = signal<Deputado[]>([]);
+  protected pesquisa = signal('');
+  protected deputadosFiltrados = computed(() => {
+
+        const texto = this.pesquisa()
+            .toLowerCase()
+            .trim();
+
+        if (!texto) {
+            return this.deputados();
+        }
+
+        return this.deputados().filter(dep =>
+            dep.nome.toLowerCase().includes(texto)
+        );
+    });
 
   constructor() {
     this.#deputadoService.obterTodos().subscribe(res=> {
       this.deputados.set(res.dados);
     });     
+  }
+  protected pesquisar(event: Event) {
+
+      const input = event.target as HTMLInputElement;
+
+      this.pesquisa.set(input.value);
   }
 }
